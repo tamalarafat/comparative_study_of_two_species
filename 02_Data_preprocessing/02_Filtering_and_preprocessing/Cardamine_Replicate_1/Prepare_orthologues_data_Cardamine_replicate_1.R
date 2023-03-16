96bdc1b8bcd6401ff1750c4aa9051be406064ebf
ytamal2 <ytamal2@mpipz.mpg.de>
2023-03-16 19:21:59 +0100

    Thursday commit :: Preprocessing of data, and preparing for integration

--- 02_Data_preprocessing/02_Filtering_and_preprocessing/Cardamine_Replicate_1/Prepare_orthologues_data_Cardamine_replicate_1.R
+++ 02_Data_preprocessing/02_Filtering_and_preprocessing/Cardamine_Replicate_1/Prepare_orthologues_data_Cardamine_replicate_1.R
@@ -10,7 +10,7 @@ sapply(scripts_list, source, .GlobalEnv)
 ortho_table = read.csv("/netscratch/dep_tsiantis/grp_laurent/tamal/2022/Input_files/Additional_inputs/Orthologues_n_correspondence/Orthos_table.csv")
 
 # Loading data -  loading of sparse data matrices provided by 10X genomics using Seurat's "Read10X" function
-OX_data_3E <- Read10X(data.dir = "/netscratch/dep_tsiantis/common/scRNAseq/FINAL_datasets_200822/outs_Ox_RNA_3rd_ALL_3000_Newest/filtered_feature_bc_matrix/")
+OX_data_1E <- Read10X(data.dir = "/netscratch/dep_tsiantis/common/scRNAseq/FINAL_datasets_200822/outs_OX_RNA_1ST_2_Newest/filtered_feature_bc_matrix/")
 
 # Convert the gene ids in the count matrix to desired orthologues gene ids
 
@@ -19,6 +19,6 @@ OX_data_3E <- Read10X(data.dir = "/netscratch/dep_tsiantis/common/scRNAseq/FINAL
 # parameters :: ortho_column_name_of_gene_ids - The name of the column in the orthologues table that contains the gene IDs that we want to convert.
 # parameters :: ortho_column_name_to_assign - The name of the column in the orthologues table that contains the gene IDs that we want to assign.
 
-Ortho_OX_REP_3 = prepare_ortho_data(input_data = OX_data_3E, ortho_data = ortho_table, ortho_column_name_of_gene_ids = "C.hirsutaOX", ortho_column_name_to_assign = "A.thaliana.TAIR10")
+Ortho_OX_REP_1 = prepare_ortho_data(input_data = OX_data_1E, ortho_data = ortho_table, ortho_column_name_of_gene_ids = "C.hirsutaOX", ortho_column_name_to_assign = "A.thaliana.TAIR10")
 
-# save(Ortho_OX_REP_3, file = "Cardamine_REP_3_Ortho_data.RData")
+# save(Ortho_OX_REP_1, file = "Cardamine_REP_1_Ortho_data.RData")
