009d56e89831813d76cd0ee1184a95f61336cbb3
Yasir <tamalarafat@gmail.com>
2023-08-23 03:59:22 +0200

    Adding cluster specific conserved markers and cluter specific markers

--- Analysis_with_Liger/07_Analysis_01_initial_downstream_analysis/06_Cluster_specific_marker_selection/01_Select_cluster_specific_conserved_markers.R
+++ Analysis_with_Liger/07_Analysis_01_initial_downstream_analysis/06_Cluster_specific_marker_selection/01_Select_cluster_specific_conserved_markers.R
@@ -9,16 +9,10 @@ grp_col = c("#FD6467", "#00A08A", "#F98400", "#046C9A", "#075149FF", "#FFA319FF"
 # Dir - containing the DEG files
 DEG_dir = "/home/yasir/Documents/Projects_Yasir/comparative_study_of_two_species/Analysis_with_Liger/07_Analysis_01_initial_downstream_analysis/Analysis_outputs/Differentially_expressed_genes/Conserved_DEGs_grouped_by_Species/Conserved_markers_DEtest_wilcox"
 
-DEG_files = str_sort(list.files(DEG_dir, pattern = "Cluster_"), numeric = TRUE)
-
-Cluster_DEGs = lapply(str_c(DEG_dir, "/", DEG_files), loadRData)
-
-temp_names = names(Cluster_DEGs)
-
 # Storing directory
 storing_dir = "/home/yasir/Documents/Projects_Yasir/comparative_study_of_two_species/Analysis_with_Liger/07_Analysis_01_initial_downstream_analysis/Analysis_outputs"
 
-specific_markers_list = specific_conserved_marker_finder(DEG_file = Cluster_DEGs, 
+specific_markers_list = specific_conserved_marker_finder(DEG_file = DEG_dir, 
                                                          max.pct2.detection = 0.1, 
                                                          include.pct.difference = TRUE, 
                                                          pct.difference = 0.3, 
--- Analysis_with_Liger/07_Analysis_01_initial_downstream_analysis/06_Cluster_specific_marker_selection/02_Select_cluster_specific_markers.R
+++ Analysis_with_Liger/07_Analysis_01_initial_downstream_analysis/06_Cluster_specific_marker_selection/02_Select_cluster_specific_markers.R
@@ -7,9 +7,13 @@ grp_col = c("#FD6467", "#00A08A", "#F98400", "#046C9A", "#075149FF", "#FFA319FF"
 
 # Markers from cluster DEGs
 # Dir - containing the DEG files
-DEG_dir = "/home/yasir/Documents/Projects_Yasir/Analysis_of_single_species_Cardamine/Analysis_with_Liger/Analysis_02_after_controlling_for_batch_effects/Analysis_outputs/Differentially_expressed_genes/DEGs_of_each_cluster/DEG_DEtest_wilcox"
+DEG_dir = "/home/yasir/Documents/Projects_Yasir/comparative_study_of_two_species/Analysis_with_Liger/07_Analysis_01_initial_downstream_analysis/Analysis_outputs/Differentially_expressed_genes/DEGs_of_each_cluster/DEG_DEtest_wilcox"
 
 # Storing directory
-storing_dir = "/home/yasir/Documents/Projects_Yasir/Analysis_of_single_species_Cardamine/Analysis_with_Liger/Analysis_02_after_controlling_for_batch_effects/Analysis_outputs"
+storing_dir = "/home/yasir/Documents/Projects_Yasir/comparative_study_of_two_species/Analysis_with_Liger/07_Analysis_01_initial_downstream_analysis/Analysis_outputs"
 
-specific_markers_list = specific_marker_finder(DEG_file_dir = DEG_dir, pct_detection = 0.1, pct_diff = 0.3, store_dir = storing_dir)
+specific_markers_list = specific_marker_finder(DEG_file_dir = DEG_dir, 
+                                               pct_detection = 0.1, 
+                                               pct_diff = 0.3, 
+                                               store_dir = storing_dir, 
+                                               store.marker.file = TRUE)
